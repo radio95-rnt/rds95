e7b4a79ce33c14462a1ff5344cbf3d1d67fd8610
KubaPro010 <kuba.pro010@gmail.com>
2025-12-22 21:53:49 +0100

    that was tuff

--- src/lua_rds.c
+++ src/lua_rds.c
@@ -12,10 +12,7 @@ int lua_set_rds_program_defaults(lua_State *localL) {
 
 int lua_reset_rds(lua_State *localL) {
     (void)localL;
-    if(encoder_saveToFile(mod->enc)) {
-        lua_set_rds_program_defaults(localL);
-        encoder_saveToFile(mod->enc)
-    }
+    encoder_saveToFile(mod->enc);
 	encoder_loadFromFile(mod->enc);
 	for(int i = 0; i < PROGRAMS; i++) reset_rds_state(mod->enc, i);
     Modulator_saveToFile(&mod->params);
