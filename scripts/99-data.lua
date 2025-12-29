246310779f5f7184745b030d8148c69854b07a4d
KubaPro010 <kuba.pro010@gmail.com>
2025-12-29 11:10:16 +0100

    multiple arguments

--- scripts/99-data.lua
+++ scripts/99-data.lua
@@ -2,11 +2,11 @@ function data_handle(data)
     if string.sub(data, 1, 4):lower() == "lua=" then
         local chunk, err = load("return " .. string.sub(data, 5), "udp_chunk")  -- skip "lua="
         if not chunk then return string.format("-\r\n%s\r\n", err) end
-        return tostring(chunk()) .. "\r\n"
+        return table.concat({ chunk() }, ", ") .. "\r\n"
     elseif string.sub(data, 1, 5):lower() == "file=" then
         local chunk, err = loadfile(data:sub(6))  -- skip "file="
         if not chunk then return string.format("-\r\n%s\r\n", err) end
-        return tostring(chunk()) .. "\r\n"
+        return table.concat({ chunk() }, ", ") .. "\r\n"
     end
 
     local cmd, value = data:match("([^=]+)=([^=]+)")
