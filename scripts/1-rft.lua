fd2824d18ba8178b90f3e9859a26b1e7553db011
KubaPro010 <kuba.pro010@gmail.com>
2025-12-31 20:42:10 +0100

    no clue

--- scripts/1-rft.lua
+++ scripts/1-rft.lua
@@ -68,10 +68,7 @@ function RftInstance:start()
 
             local total_segments = math.ceil(#self.file_data / 5)
 
-            local last_stream = false
-            if stream == (math.min(get_available_rds_streams(), get_rds_streams()) - 1) then last_stream = true end
-
-            if not self.crc_sent_this_cycle and self.crc_enabled and (self.file_segment % 16 == 0) and (not last_stream) then
+            if not self.crc_sent_this_cycle and self.crc_enabled and (self.file_segment % 16 == 0) then
                 self.crc_sent_this_cycle = true
                 local chunk_address = math.floor((self.crc_segment - 1) / 2)
                 local c = (1 << 12) | (self.crc_mode & 7) << 9 | (chunk_address & 0x1ff)
